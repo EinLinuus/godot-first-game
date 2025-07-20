9ec7ea08a8025f16f4e200da5c10b39320b835d6
EinLinuus <github@linusbenkner.de>
2025-07-20 02:20:01 +0200

    feat: on die: slow game down and make player fall

--- scenes/killzone.gd
+++ scenes/killzone.gd
@@ -13,8 +13,11 @@ func _process(delta: float) -> void:
 
 
 func _on_body_entered(body: Node2D) -> void:
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
