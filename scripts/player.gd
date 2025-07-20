329e8615c4831dffb2fcc1856f05fbce90d0e8b7
EinLinuus <github@linusbenkner.de>
2025-07-20 02:30:35 +0200

    feat: add player animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 100.0
 const JUMP_VELOCITY = -250.0
 
+@onready var player = $AnimatedSprite2D
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -14,9 +16,20 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
+	if direction > 0:
+		player.flip_h = false
+	elif direction < 0:
+		player.flip_h = true
+		
+	if is_on_floor():
+		if direction == 0:
+			player.play('idle')
+		else:
+			player.play('run')
+	else:
+		player.play('jump')
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
